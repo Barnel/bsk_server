69ab368921829508f9340a88beb788ed088801fd
Grzegorz Cieśla <grzegorz.ciesla96@gmail.com>
2019-06-08 13:06:18 +0200

    Add gitignore, saving files directory change

--- app.ts
+++ app.ts
@@ -2,22 +2,20 @@
 import * as express from "express"
 import * as http from "http"
 import * as fs from "fs"
-// let bodyParser = require('body-parser')
-let busboy = require('connect-busboy')
+let bodyParser = require('body-parser')
 const app = express();
-app.use(busboy());
 
 const httpServer = new http.Server(app)
 
-// app.use(bodyParser({keepExtension: true}));
+app.use(bodyParser({keepExtension: true}));
 
 
 app.get('/:fileName', function(req, res){
-    res.download(`./${req.params.fileName}`);
+    res.download(`./uploads/${req.params.fileName}`);
 });
 
 app.post('/:fileName', function(req, res){
-    fs.writeFile(`./${req.params.fileName}`, JSON.stringify(req.body), (err) => {
+    fs.writeFile(`./uploads/${req.params.fileName}`, JSON.stringify(req.body), (err) => {
         if(err) {
             console.log(err)
         }
